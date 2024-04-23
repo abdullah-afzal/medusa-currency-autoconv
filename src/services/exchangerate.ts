68e44d2ddec86930474866cf78975262244efeb5
medbenzekri <medbenzekri219@gmail.com>
2024-04-23 10:53:39 +0100

    fixed rates issue

--- src/services/exchangerate.ts
+++ src/services/exchangerate.ts
@@ -1,23 +1,27 @@
 import { TransactionBaseService, Currency, Logger } from "@medusajs/medusa";
-import CurrencyExchangeRateRepository from "../repositories/exchangerate";
-import { CurrencyExchangeRate } from "../models/exchangerates";
-import FreeCurrencyApi from "@everapi//freecurrencyapi-js";
+import CurrencyExchangeRateRepository from "../repositories/currency-exchange-rate";
+import { CurrencyExchangeRate } from "../models/currency-exchange-rate";
+import Freecurrencyapi from "../helpers/freecurrencyapi";
 import {safeRate} from "../helpers/rate";
 import { In } from 'typeorm';
+import { PluginOptions } from "../types/types";
 
 
 
 class ExchangeRateService extends TransactionBaseService {
     private logger: Logger;
-    private api: FreeCurrencyApi;
+    private api:  Freecurrencyapi;
     private apiKey: string;
     private currencyExchangeRateRepository: typeof CurrencyExchangeRateRepository;
+    private pluginOptions : PluginOptions;
 
-    constructor(container, options) {
+    constructor(container, pluginOptions) { 
+        
         super(container);
+        this.pluginOptions = pluginOptions;
         this.logger = container.logger;
-        this.apiKey = options.apiKey;
-        this.api = new FreeCurrencyApi(this.apiKey);
+        this.apiKey = this.pluginOptions.api_key;
+        this.api = new Freecurrencyapi(this.apiKey);
         this.currencyExchangeRateRepository = container.currencyExchangeRateRepository;
     }
     /**
@@ -25,8 +29,8 @@ class ExchangeRateService extends TransactionBaseService {
     */
     async createRate(baseCurrency: Currency, targetCurrency: Currency): Promise<void> {
         try {
-            const response = await this.api.latest(baseCurrency, [targetCurrency]);
-            const rate = response.data[targetCurrency];
+            const response = await this.api.latest({base_currency:  baseCurrency,currencies: [targetCurrency.code]});
+            const rate = response.data[targetCurrency.code];
             this.currencyExchangeRateRepository.create({ currency: targetCurrency, rate });
         } catch (error) {
             this.logger.error('Failed to create exchange rate:', error);
@@ -37,24 +41,34 @@ class ExchangeRateService extends TransactionBaseService {
 
     async upsertRate(baseCurrency: Currency,  targetCurrencies: Currency[],buffer: number = 0.05): Promise<Partial<CurrencyExchangeRate>[]> {
         try {
-            const currentRates = await this.currencyExchangeRateRepository.find({ where: { currency: In(targetCurrencies) } });
+            const targetCurrenciesCodes = targetCurrencies.map((currency) => currency.code);
+            const currentRates = await this.currencyExchangeRateRepository.find({ where: { code: In(targetCurrenciesCodes) }, relations: ["currency"]});
             const ratesToUpdate = currentRates.filter((rate) => {
                 const isexpired = new Date(rate.expires_at).getTime() - new Date().getTime() < 0
                 return isexpired;
             });
             const ratesToCreate = targetCurrencies.filter((currency) => {
-                return !currentRates.some((rate) => rate.currency === currency);
+                return ! currentRates.some((rate) => rate.currency === currency) ;
             });
+
+
 
             const currenciresToUpdate = ratesToUpdate.map((rate) => rate.currency.code);
             currenciresToUpdate.push(...ratesToCreate.map((rate) => rate.code));
-            const response = await this.api.latest(baseCurrency, currenciresToUpdate);
+            const response = await this.api.latest({base_currency: baseCurrency.code,currencies: currenciresToUpdate});
             const rates = Object.entries(response.data).map(([currency, rate]) => {
-                return {
-                    currency: targetCurrencies.find((targetCurrency) => targetCurrency.code === currency)
+                if(ratesToUpdate.some((rate) => rate.currency.code === currency.toLowerCase())) {
+                    return {
+                        currency: targetCurrencies.find((targetCurrency) => targetCurrency.code === currency.toLowerCase())
+                        , rate: safeRate(Number(rate), buffer)
+                        , id: ratesToUpdate.find((rate) => rate.currency.code === currency.toLowerCase()).id
+                    } as Partial<CurrencyExchangeRate>; 
+                }
+                return  this.currencyExchangeRateRepository.create( {
+                    currency: targetCurrencies.find((targetCurrency) => targetCurrency.code === currency.toLowerCase())
                     , rate: safeRate(Number(rate), buffer)
-                };
-            });
+                });
+            }) ;
             await this.currencyExchangeRateRepository.save(rates);
             return rates;
         } catch (error) {
