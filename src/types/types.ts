e77256baa84f505450196176c70a79524856af8b
medbenzekri <medbenzekri219@gmail.com>
2024-05-07 13:37:32 +0100

    added manual rates

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,8 @@
+import { CurrencyExchangeRate } from "../models/currency-exchange-rate";
 export interface PluginOptions {
     api_key: string;
     SupportedCurrencies:string[]
+    manualRates:Partial<CurrencyExchangeRate>[]
     Buffer:number
     BaseCurrency:string;
 }
